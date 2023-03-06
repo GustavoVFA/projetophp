387eea3ee32d5fcbd828feda6542d0dbe04b0cca
GustavoVFA <gustavovfamorim@gmail.com>
2023-03-06 20:11:22 -0300

    variaveis super globais

--- variaveis_globais.php
+++ variaveis_globais.php
@@ -1,5 +1,9 @@
 <?php
 
+    echo "<br/> ola seja bem vindo". $_SERVER['REMOTE_ADDR']. "<br/>";
+    echo "<br/> voce esta utilizando o navegador: ". $_SERVER['HTTPS_USER_AGENT'];
+
+
     $ano = 2020;
     //implementação de uma função
     function exibir (){
