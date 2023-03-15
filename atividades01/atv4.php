98225cdfdd04d495e373bb95d5a0923d9186b2ab
GustavoVFA <gustavovfamorim@gmail.com>
2023-03-15 19:10:14 -0300

    atv.4 finalização

--- atividades01/atv4.php
+++ atividades01/atv4.php
@@ -1,10 +1,18 @@
 <?php
-
-    $n1 = 20;
-    $n2 = 15;
+    $n1 = 30;
+    $n2 = 20;
     $n3 = 10;
-
-    if($n1 > $n2 && $n1 > $n3 && $n2 > $n3){
-        echo "$n1, $n2, $n3";
+    if($n1 >= $n2 && $n2 >= $n3){ 
+        echo "A ordem decrescente é: " .$n1. "\n" .$n2."\n".$n3; 
+    }else if ($n1 >= $n3 && $n3 >= $n2){
+        echo "A ordem decrescente é: ".$n1."\n".$n3."\n".$n2; 
+    }else if($n2 >= $n1 && $n1 >= $n3){
+        echo "A ordem decrescente é: ". $n2."\n".$n1."\n".$n3; 
+    }else if($n2 >= $n3 && $n3 >= $n1){
+        echo "A ordem decrescente é: ". $n2. "\n" .$n3."\n".$n1; 
+    }else if($n3 >= $n1 && $n1 >= $n2){
+        echo "A ordem decrescente é: ". $n3."\n".$n1."\n".$n2; 
+    }else if($n3 >= $n2 && $n2 >= $n1){
+        echo "A ordem decrescente é: ". $n3."\n".$n2."\n".$n1; 
     }
-?>
+ ?>
