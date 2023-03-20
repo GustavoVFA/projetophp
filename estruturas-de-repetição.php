92af4f969caca6ce180ae374d74479775f8aea42
GustavoVFA <gustavovfamorim@gmail.com>
2023-03-20 19:41:42 -0300

    1

---
+++ estruturas-de-repetição.php
@@ -0,0 +1,20 @@
+<?php
+
+    for($i = 0; $i <= 2; $i++){
+        for($j = 0; $j <= 2; $j++){
+            echo "$i, $j <br/>";
+        };
+
+    };
+
+    $cont = 0;
+    
+
+    do{
+        $cont++;
+        echo "O valor é : $cont <br/>";
+    }while($cont <=4);
+
+
+
+?>
