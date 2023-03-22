5f0743d383d54e8b7edc4d71d04c54ef5357b1c3
GustavoVFA <gustavovfamorim@gmail.com>
2023-03-22 19:26:13 -0300

    Finalização de arquivo ( - Resolução da atividade 1 da pasta 2 )

--- Atividades02/atv1.php
+++ Atividades02/atv1.php
@@ -1,5 +1,34 @@
 <?php
 
-    $;
+    $cod=1; $valor=10; $res=0;
+
+    switch($cod){
+        case 1 :
+            $res = $valor * 1.1;
+            echo "Valor = $valor + 10% <br/>
+            resultado = $res";
+            break;
+            
+        case 3 :
+            $res = $valor * 1.25;
+            echo "Valor = $valor + 25% <br/>
+            resultado = $res";
+            break;
+
+        case 4 :
+            $res = $valor * 1.30;
+            echo "Valor = $valor + 30% <br/>
+            resultado = $res";
+            break;
+        
+        case 8 :
+            $res = $valor * 1.50;
+            echo "Valor = $valor + 50% <br/>
+            resultado = $res";
+            break;
+
+        
+
+    };
 
 ?>
