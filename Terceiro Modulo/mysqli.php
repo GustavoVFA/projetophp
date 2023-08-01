eedd41ac36161de55dda05108131ab37249d87b2
GustavoVFA <gustavovfamorim@gmail.com>
2023-07-31 21:57:13 -0300

    Fechando conexão no banco

--- Terceiro Modulo/mysqli.php
+++ Terceiro Modulo/mysqli.php
@@ -2,7 +2,7 @@
 
 //criando coonexão com o banco de dados
 
-$con = mysqli_connect("localhost", "GustavoVFA", "g!T01-@rU6E2qG3K");
+$con = mysqli_connect("localhost", "GustavoVFA", "g!T01-@rU6E2qG3K", "db_exemplo");
 
 //Checando conexão
 
@@ -11,5 +11,15 @@ if(mysqli_connect_errno()){
     echo "Erro ao conectar com a base de dados: ".mysqli_connect_error();
 }else{
     echo "Conexão Aberta";
+    mysqli_close($con);
+    echo "<br> Conexão fechada";
 }
+
+
+
+ // Fechando Conexão
+
+//select codigo +1 from (nome tabela) order by codigo desc
+
+
 ?>
