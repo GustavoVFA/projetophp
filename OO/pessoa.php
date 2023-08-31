74f87faebedf090cf9cb52d2ac040cfc11d1892d
GustavoVFA <gustavovfamorim@gmail.com>
2023-08-30 21:30:38 -0300

    pss .php upt

--- OO/pessoa.php
+++ OO/pessoa.php
@@ -10,11 +10,22 @@ class Pessoa{
     var $nascimento;
     var $escolaridade;
     var $salario;
-
+    // aumenta a altura em centimetros
     function crescer($centimetro){
-        if($centimetro = 0){
+        if($centimetro > 0){
             $this->altura += $centimetro;
         }
     }
+
+    function formar($titulacao){
+        $this->escolaridade = $titulacao;
+    }
+    
+    function envelhecer($anos){
+        if($anos = 0){
+            $this->idade = $anos;
+        }
+    }
+
 
 }
