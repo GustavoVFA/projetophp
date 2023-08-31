a4361cbb0319c7f2c1fdabda239a45a6fbadcc1f
GustavoVFA <gustavovfamorim@gmail.com>
2023-08-30 22:07:08 -0300

    objetos.php

--- OO/exercicio2/objetos.php
+++ OO/exercicio2/objetos.php
@@ -1,1 +1,23 @@
 <?php
+
+# Criação do objeto $conta_carlos
+
+
+$conta_carlos = new Conta;
+$conta_carlos->agencia = 6677;
+$conta_carlos->codigo = "CC.12 34. 56";
+$conta_carlos->dataDeCriacao = "10/07/02";
+$conta_carlos->titular = $carlos;
+$conta_carlos->senha = 9876;
+$conta_carlos->saldo = 567.89;
+$conta_carlos->cancelada = false;
+
+echo "</br>";
+echo "Manipulando a conta de: {$conta_carlos->titular->nome} </br>";
+echo "O saldo atual é R\$ {$conta_carlos->obterSaldo()} </br>";
+
+$conta_carlos->depositar(20);
+echo "O saldo atual é R\$ {$conta_carlos->obterSaldo()} </br>";
+
+$conta_carlos->retirar(10);
+echo "O saldo atual é R\$ {$conta_carlos->obterSaldo()} </br>";
