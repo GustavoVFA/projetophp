19e6932665e03c87cfaf80f442ecf7ba63a594d7
GustavoVFA <gustavovfamorim@gmail.com>
2023-10-04 14:53:20 -0300

    funcionario.php

---
+++ Estrutura_de_classes/funcionario.php
@@ -0,0 +1,60 @@
+<?php 
+
+
+class Funcionario{
+    private $codigo;
+    private $nome;
+    private $nascimento;
+    protected $salario;
+
+
+    
+    function setCodigo($codigo){
+
+        $this->codigo = $codigo;
+
+    }
+
+    function getCodigo(){
+
+        return $this->codigo;
+
+    }
+
+
+    
+    function setNome($nome){
+        
+        $this->nome = $nome;
+        
+    }
+    
+    
+    function getNome(){
+        return $this->nome;
+    }
+    
+    
+    function setNascimento($nascimento){
+        $this->nascimento = $nascimento;
+    }
+    
+    function getNascimento(){
+        return $this->nascimento;
+    }
+    
+    function setSalario($salario){
+    
+        if(is_numeric($salario)&&($salario > 0)){
+            $this->salario = $salario;
+        }    
+    }
+    
+    
+    function getSalario(){
+        return $this->salario;
+    }
+    
+}
+
+?>
